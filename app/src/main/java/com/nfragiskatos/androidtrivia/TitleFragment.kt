b15acc2b2dd5d0a66318b189e4d1eafb6c360f26
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2019-10-03 16:46:01 -0700

    Added safe-args for passing bundles between fragments
    Switched fragments to navigate using generated *FragmentDirections action instead of resource id.
    
    Added numQuestions and numCorrect arguments to game fragment
    
    Displayed arguments in game won fragment in a Toast

--- app/src/main/java/com/nfragiskatos/androidtrivia/TitleFragment.kt
+++ app/src/main/java/com/nfragiskatos/androidtrivia/TitleFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.nfragiskatos.androidtrivia.databinding.FragmentTitleBinding
@@ -19,11 +18,10 @@ class TitleFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
 
-        binding.playButton.setOnClickListener (
-                Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
-        )
+        binding.playButton.setOnClickListener { v: View ->
+            v.findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment())
+        }
         setHasOptionsMenu(true)
-
         return binding.root
     }
 
